4ebb4cdb38e7f7b96d71854b4c51840525ec24c2
RedMachine8 <redmachine8@yandex.ru>
2025-04-23 15:45:30 +0300

    Выполненное задание повышенной сложности 1

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,14 +1,18 @@
 const timerElement = document.getElementById('timer');
 let startTimer = parseInt(timerElement.textContent);
 
-function updateTimer() {
-    startTimer--;
-    timerElement.textContent = startTimer;
+function pad(num) {
+    return String(num).padStart(2, '0');
+}
 
+function updateTimer() {
+    const hhTimer = new Date(startTimer * 1000).toISOString().slice(11, 19);
+    timerElement.textContent = hhTimer;
     if(startTimer === 0) {
         clearInterval(timerInterval);
         alert('Вы победили в конкурсе!');
     }
+    startTimer--;
 }
 
 const timerInterval = setInterval(updateTimer, 1000);
