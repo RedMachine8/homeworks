cc0b7985b0b8a9c594d9a344a76563d9f926c1af
Влад Сергеев <vladsergeev@iMac-Vlad.local>
2025-07-01 15:56:59 +0300

    Добавление задания "Авторизация"

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,14 +1,16 @@
 const welcome = document.getElementById('welcome');
 const form = document.getElementById('signin__form');
+const signInForm = document.getElementById('signin');
 const userIdSpan = document.getElementById('user_id');
 const xhr = new XMLHttpRequest();
 
-form.addEventListener('submit', () => {
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
     const login = form.elements.login.value;
     const password = form.elements.password.value;
     const dataUser = JSON.stringify({
         login: login,
-        password: password 
+        password: password
     });
 
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
@@ -16,25 +18,21 @@ form.addEventListener('submit', () => {
     xhr.send(dataUser);
 })
 
-xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-            const data = JSON.parse(xhr.responseText);
-            if(data.success) {
-                localStorage.setItem('user_id', user_id);
-                welcome.classList.add('welcome_active');
-                form.classList.remove('signin_active')
-                userIdSpan.textContent = data.user_id;
-            } else {
-                alert('Неверный логин/пароль');
-            }
-        }
+xhr.onload = function() {
+    const data = JSON.parse(xhr.responseText);
+    if(data.success) {
+        localStorage.setItem('user_id', data.user_id);
+        welcome.classList.add('welcome_active');
+        signInForm.classList.remove('signin_active')
+        userIdSpan.textContent = data.user_id;
+    } else {
+        alert('Неверный логин/пароль');
     }
 };
 
 const storeUserId = localStorage.getItem('user_id');
 if(storeUserId) {
-    welcomeBlock.classList.add('welcome_active');
+    welcome.classList.add('welcome_active');
     userIdSpan.textContent = storeUserId;
-    signinForm.classList.remove('signin_active');
+    signInForm.classList.remove('signin_active');
 }
